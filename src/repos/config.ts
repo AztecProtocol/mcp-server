ef51069db12adcdc3225182163cd82a42df05c78
Josh Crites <jc@joshcrites.com>
2026-03-30 15:42:45 -0400

    feat: update default Aztec version to v4.2.0-aztecnr-rc.2

--- src/repos/config.ts
+++ src/repos/config.ts
@@ -29,7 +29,7 @@ export interface RepoConfig {
 }
 
 /** Default Aztec version (tag) to use - can be overridden via AZTEC_DEFAULT_VERSION env var */
-export const DEFAULT_AZTEC_VERSION = process.env.AZTEC_DEFAULT_VERSION || "4.2.0-aztecnr-rc.2";
+export const DEFAULT_AZTEC_VERSION = process.env.AZTEC_DEFAULT_VERSION || "v4.2.0-aztecnr-rc.2";
 
 /**
  * Base Aztec repository configurations (without version)
